017cf9384418d017e1f291de13fe7af20f5fbead
super-jb <john.acb@gmail.com>
2021-11-29 23:55:00 -0800

    Integration with Ordering.Api

---
+++ src/Services/Ordering/Ordering.API/EventBusConsumers/BasketCheckoutConsumer.cs
@@ -0,0 +1,30 @@
+﻿using Ardalis.GuardClauses;
+using AutoMapper;
+using EventBus.Messages.Events;
+using MassTransit;
+using MediatR;
+using Ordering.Application.Features.Orders.Commands.CheckoutOrder;
+
+namespace Ordering.API.EventBusConsumers;
+
+public class BasketCheckoutConsumer : IConsumer<BasketCheckoutEvent>
+{
+    private readonly IMediator _mediator;
+    private readonly IMapper _mapper;
+    private readonly ILogger<BasketCheckoutConsumer> _logger;
+
+    public BasketCheckoutConsumer(IMediator mediator, IMapper mapper, ILogger<BasketCheckoutConsumer> logger)
+    {
+        _mediator = Guard.Against.Null(mediator, nameof(mediator));
+        _mapper = Guard.Against.Null(mapper, nameof(mapper));
+        _logger = Guard.Against.Null(logger, nameof(logger));
+    }
+
+    public async Task Consume(ConsumeContext<BasketCheckoutEvent> context)
+    {
+        CheckoutOrderCommand command = _mapper.Map<CheckoutOrderCommand>(context.Message);
+        int orderId = await _mediator.Send(command);
+
+        _logger.LogInformation($"BasketCheckoutEvent consumed successfully. Created Order Id : {orderId}");
+    }
+}

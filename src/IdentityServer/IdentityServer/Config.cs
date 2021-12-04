79043e0aa765c6613062f3bc386a43f9d6df0c95
super-jb <john.acb@gmail.com>
2021-12-03 23:12:32 -0800

    Adding IdentityServer project to generate JWT

---
+++ src/IdentityServer/IdentityServer/Config.cs
@@ -0,0 +1,83 @@
+﻿using IdentityModel;
+using IdentityServer4;
+using IdentityServer4.Models;
+using IdentityServer4.Test;
+using System.Security.Claims;
+
+namespace IdentityServer;
+
+public class Config
+{
+    public static IEnumerable<Client> Clients =>
+        new Client[]
+        {
+            new Client
+            {
+                ClientId = "shoppingClient",
+                AllowedGrantTypes = GrantTypes.ClientCredentials,
+                ClientSecrets = { new Secret("secret".Sha256(), expiration: DateTime.UtcNow.AddHours(1)) },
+                AllowedScopes = { "shoppingApi" }
+            },
+            new Client
+            {
+                ClientId = "shopping_mvc_client",
+                ClientName = "Shopping MVC Web App",
+                AllowedGrantTypes = GrantTypes.Hybrid,
+                RequirePkce = false,
+                AllowRememberConsent = false,
+                RedirectUris = new List<string> { "https://localhost:5010/signin-oidc" }, // TODO: verify this url
+                PostLogoutRedirectUris = new List<string> { "https://localhost:5010/signout-callback-oidc" }, // TODO: verify this url
+                ClientSecrets = new List<Secret> { new Secret("secret".Sha256(), expiration: DateTime.UtcNow.AddHours(1)) },
+                AllowedScopes = new List<string>
+                {
+                    IdentityServerConstants.StandardScopes.OpenId,
+                    IdentityServerConstants.StandardScopes.Profile,
+                    IdentityServerConstants.StandardScopes.Address,
+                    IdentityServerConstants.StandardScopes.Email,
+                    "shoppingApi",
+                    "roles"
+                }
+            }
+        };
+
+    public static IEnumerable<ApiScope> ApiScopes =>
+       new ApiScope[]
+       {
+            new ApiScope("shoppingApi", "Shopping API")
+       };
+
+    public static IEnumerable<ApiResource> ApiResources =>
+        new ApiResource[]
+        {
+            new ApiResource("shoppingApi", "Shopping API")
+        };
+
+    public static IEnumerable<IdentityResource> IdentityResources =>
+        new IdentityResource[]
+        {
+            new IdentityResources.OpenId(),
+            new IdentityResources.Profile(),
+            new IdentityResources.Address(),
+            new IdentityResources.Email(),
+            new IdentityResource(
+                "roles",
+                "Your role(s)",
+                new List<string>() { "role" })
+        };
+
+    public static List<TestUser> TestUsers =>
+        new()
+        {
+            new TestUser
+            {
+                SubjectId = "5BE86359-073C-434B-AD2D-A3932222DABE",
+                Username = "jb",
+                Password = "password",
+                Claims = new List<Claim>
+                {
+                    new Claim(JwtClaimTypes.GivenName, "john"),
+                    new Claim(JwtClaimTypes.FamilyName, "b")
+                }
+            }
+        };
+}
---
+++ src/IdentityServer/IdentityServer/Program.cs
@@ -0,0 +1,35 @@
+using IdentityServer;
+
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddIdentityServer()
+    .AddInMemoryClients(Config.Clients)
+    .AddInMemoryApiScopes(Config.ApiScopes)
+    .AddInMemoryIdentityResources(Config.IdentityResources)
+    .AddTestUsers(Config.TestUsers)
+    .AddDeveloperSigningCredential();
+
+builder.Services.AddRazorPages();
+
+WebApplication app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+app.UseIdentityServer();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.Run();
